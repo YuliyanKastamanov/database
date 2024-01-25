114e6de1b32ac18c857e7a9a58b8e97f2636c66e
YuliyanKastamanov <92514049+YuliyanKastamanov@users.noreply.github.com>
2024-01-25 17:33:35 +0200

    Remember me implementation.

--- database/src/main/java/databaseApp/db/config/SecurityConfiguration.java
+++ database/src/main/java/databaseApp/db/config/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package databaseApp.db.config;
 
+import databaseApp.db.model.entity.RoleEntity;
+import databaseApp.db.model.entity.enums.RoleEnum;
 import databaseApp.db.repository.UserRepository;
 import databaseApp.db.service.impl.DbUserDetailsService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,50 +20,67 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @Configuration
 public class SecurityConfiguration {
 
+    private final String rememberMeKey;
+
+    public SecurityConfiguration(@Value("${databaseApp.remember.me.key}") String rememberMeKey) {
+        this.rememberMeKey = rememberMeKey;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(
+        return httpSecurity.authorizeHttpRequests(
 
-                // define which urls are visible by which users
-                authorizeRequest -> authorizeRequest
-                        //all static resources are situated in (folders name) are available for anyone
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        //Allow anyone to see the home page and login page and signup page
-                        .requestMatchers("/", "/users/login", "/users/signup", "/users/login-error").permitAll()
-                        // all other request are authenticated
-                        .anyRequest().authenticated()
+                        // define which urls are visible by which users
+                        authorizeRequest -> authorizeRequest
+                                //all static resources are situated in (folders name) are available for anyone
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                //Allow anyone to see the home page and login page and signup page
+                                .requestMatchers("/", "/users/login", "/users/signup", "/users/login-error").permitAll()
+                                .requestMatchers("/db/review").hasRole(RoleEnum.ADMIN.name())
+                                // all other request are authenticated
+                                .anyRequest().authenticated()
 
 
-        ).formLogin(
-                formLogin -> {
-                    formLogin
-                            //redirect here when we access something which is not allowed.
-                            //this is the page where we perform login
-                            .loginPage("/users/login")
+                ).formLogin(
+                        formLogin -> {
+                            formLogin
+                                    //redirect here when we access something which is not allowed.
+                                    //this is the page where we perform login
+                                    .loginPage("/users/login")
 
-                            //The names of the input fields
-                            .usernameParameter("uNumber")
-                            .passwordParameter("password")
-                            .defaultSuccessUrl("/")
-                            .failureForwardUrl("/users/login-error");
-                    //.failureForwardUrl("/users/login-error");
-                }
-        ).logout(
-                logout -> {
+                                    //The names of the input fields
+                                    .usernameParameter("uNumber")
+                                    .passwordParameter("password")
+                                    .defaultSuccessUrl("/")
+                                    .failureForwardUrl("/users/login-error");
+                            //.failureForwardUrl("/users/login-error");
+                        }
+                ).logout(
+                        logout -> {
 
-                    logout
-                            //the url where we should POST something in order to perform logout
-                            .logoutUrl("/users/logout")
-                            //where to go when logged out
-                            .logoutSuccessUrl("/")
-                            //invalidate the HTTP session
-                            .invalidateHttpSession(true);
-                }
-        ).csrf(AbstractHttpConfigurer::disable);
+                            logout
+                                    //the url where we should POST something in order to perform logout
+                                    .logoutUrl("/users/logout")
+                                    //where to go when logged out
+                                    .logoutSuccessUrl("/")
+                                    //invalidate the HTTP session
+                                    .invalidateHttpSession(true);
+                        }
+                ).csrf(AbstractHttpConfigurer::disable).
+                rememberMe(
 
-        //TODO remember me!
-        return httpSecurity.build();
+                        rememberMe -> {
+                            rememberMe
+                                    .key(rememberMeKey)
+                                    .rememberMeParameter("rememberme")
+                                    .rememberMeCookieName("rememberme");
+
+                        }
+                )
+                .build();
+
+
+
     }
 
     @Bean
--- database/src/main/java/databaseApp/db/service/impl/DbUserDetailsService.java
+++ database/src/main/java/databaseApp/db/service/impl/DbUserDetailsService.java
@@ -1,7 +1,10 @@
 package databaseApp.db.service.impl;
 
+import databaseApp.db.model.entity.RoleEntity;
 import databaseApp.db.model.entity.UserEntity;
 import databaseApp.db.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -22,19 +25,27 @@ public class DbUserDetailsService implements org.springframework.security.core.u
 
         return userRepository
                 .findByuNumber(uNumber)
-                .map(this::map)
+                .map(DbUserDetailsService::map)
                 .orElseThrow(() -> new UsernameNotFoundException("User " + uNumber + " not found!"));
 
     }
 
-    private UserDetails map(UserEntity userEntity) {
+    private static UserDetails map(UserEntity userEntity) {
 
         return User
                 .withUsername(userEntity.getuNumber())
                 .password(userEntity.getPassword())
-                .authorities(List.of())//todo add roles
+                .authorities(userEntity.getRoles().stream().map(DbUserDetailsService::map).toList() )
                 .build();
 
+
+    }
+
+    private static GrantedAuthority map(RoleEntity roleEntity) {
+
+        return new SimpleGrantedAuthority(
+                "ROLE_" + roleEntity.getRole().name()
+        );
 
     }
 }
