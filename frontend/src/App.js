812f4f7c32e01e38c2bbbdbd92823664d4fdc5e4
YuliyanKastamanov <92514049+YuliyanKastamanov@users.noreply.github.com>
2025-08-14 23:54:29 +0300

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/tasks/check/status", {
+      method: "GET",
+      credentials: "include", //cookies
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then(data => setData(data))
+      .catch(err => console.error("Error: ", err));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>React + Spring Boot</h1>
+        <pre>{data ? JSON.stringify(data, null, 2) : "Hello from spring boot..."}</pre>
+      </header>
+    </div>
+  );
+}
+
+export default App;
